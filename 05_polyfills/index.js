e8c00d2fcaf7aa808e8017a8af6223163cd03c11
Masum Alam <masum.kare.alam@gmail>
2025-08-12 15:04:34 +0530

    working on datatype and writting blogs

---
+++ 05_polyfills/index.js
@@ -0,0 +1,10 @@
+Array.prototype.myMap = function(callback, thisArg){
+    if(Array.prototype.myMap){
+        //taking empty arr
+        let result = [];
+        for(let i = 0; i < i.lenght; i++){
+            
+            
+        }
+    }
+}
